10f41416b7caeeda74da0600b3732a7de3a9a9f0
leoscarton <leonardo.scarton29@gmail.com>
2024-09-25 23:59:00 -0300

    Deleted vision_api.py

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
 import video_capture
 import user_interface
-import vision_api
 import cv2
 import sys
 from PySide6.QtWidgets import QApplication
 
-caller = vision_api.APICaller()
-
 app = QApplication(sys.argv)
-#login_window = user_interface.LoginWindow()
-#login_window.show()
 video_window = user_interface.VideoWindow()
 video_window.show()
 app.exec()
-
-'''
-while True:
-    user_data = login_window.retrieve_user_data()
-    caller.set_user_data(user_data)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-'''
